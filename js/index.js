75a37994cb95c92e3436862ffc958a2a5fe188bf
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-09-15 19:45:28 -0300

    fix bug of jquery to skills

--- js/index.js
+++ js/index.js
@@ -1,24 +1,23 @@
-function recarregarPagina() {
+function reloadPage() {
     // Sem redimencionamento à 100ms!
     jQuery(document).ready(function () {
-        if ($(window).width() <= 1000) {
-            $(".masterdiv").hide();
+        if ($(window).width() <= 990) {
+            $(".hide-show").hide();
         } else {
-            $(".masterdiv").show();
+            $(".hide-show").show();
         }
     });
     jQuery(document).ready(function () {
-        if ($(window).width() <= 1000 && ($("#carouselExampleControls-media").parent().is($(".container"))) )
+        if ( ($(window).width() <= 990) && ( $("#carouselExampleControls-media").parent().is($("#carouselExampleControls"))) )
             $("#carouselExampleControls-media").unwrap();
-        else if ( $("#carouselExampleControls-media").parent().is($("#carouselExampleControls"))){
-        }
-        else{
+        else if ($("#carouselExampleControls-media").parent().is($(".container"))){
             $("#carouselExampleControls-media").wrap("<div id=\"carouselExampleControls\" class=\"carousel slide\" data-ride=\"carousel\"></div>");
         }
     });
 }
-var boraLa;
+
+var run_responsive;
 window.onresize = function(){
-  clearTimeout(boraLa);
-  boraLa = setTimeout(recarregarPagina, 100);
+  clearTimeout(run_responsive);
+  run_responsive = setTimeout(reloadPage, 100);
 };
