fe95878cfb72b3057a0db9079fe3e12676c17224
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-09-16 10:57:14 -0300

    fix bug of jquery to skills

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+jQuery(document).ready(function () {
+        if ($(window).width() <= 990) {
+            $(".hide-show").hide();
+        } else {
+            $(".hide-show").show();
+        }
+});
+jQuery(document).ready(function () {
+    if ($(window).width() <= 990) {
+        if(!($("#carouselExampleControls-media").parent().is($(".container"))))
+            $("#carouselExampleControls-media").unwrap();
+    }
+    else if ($("#carouselExampleControls-media").parent().is($(".container"))){
+        $("#carouselExampleControls-media").wrap("<div id=\"carouselExampleControls\" class=\"carousel slide\" data-ride=\"carousel\"></div>");
+    }
+});
+
 function reloadPage() {
     jQuery(document).ready(function () {
         if ($(window).width() <= 990) {
